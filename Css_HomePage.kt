812f7e7790e108d9b57e8f7bef71599a95286e03
Ucuts <yusufsecrete55214@gmail.com>
2024-06-12 21:38:15 +0800

    Add files via upload

---
+++ Css_HomePage.kt
@@ -0,0 +1,21 @@
+package com.yusuf_222003.e_learning.CSS
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yusuf_222003.e_learning.R
+
+class Css_HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_css_home_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- Html_HomePage.kt
+++ Html_HomePage.kt
@@ -1,6 +1,9 @@
 package com.yusuf_222003.e_learning.HTML
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +11,23 @@ import androidx.core.view.WindowInsetsCompat
 import com.yusuf_222003.e_learning.R
 
 class Html_HomePage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_html_home_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val view2 = findViewById<View>(R.id.view2)
+        val textview14 = findViewById<TextView>(R.id.textView14)
+
+        view2.setOnClickListener {
+            // Ketika View di klik, buka halaman HTML
+            startActivity(Intent(this, Html_pengertian::class.java))
         }
+
+        textview14.setOnClickListener {
+            // Ketika TextView di klik, buka halaman HTML
+            startActivity(Intent(this, Html_pengertian::class.java))
+        }
+
     }
 }
